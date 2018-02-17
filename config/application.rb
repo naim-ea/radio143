a58a95e2d81a8ca107d860c544c82a73b59748df
naim-ea <elayadinaim@gmail.com>
2018-02-17 16:51:23 +0100

    :sparkles: New database elements

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RADIO143
+module Radio143
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
---
+++ db/migrate/20180217134731_add_role_to_users.rb
@@ -0,0 +1,6 @@
+class AddRoleToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :role, :string, default: "common"
+    add_index :users, :role
+  end
+end
