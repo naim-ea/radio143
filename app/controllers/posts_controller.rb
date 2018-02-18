7fac6d7502e0119c4e4599b2558f3f75beee2e5e
naim-ea <elayadinaim@gmail.com>
2018-02-18 15:11:27 +0100

    :sparkles: Added views and routes for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+    end
+
+    def show;end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        post = Post.create(params[:post].permit!)
+        redirect_to root_path if post.persisted?
+    end
+
+    def destroy
+        @post.destroy
+    end
+
+    private
+
+    def find_post
+        @post = Post.find(params[:id])
+    end
+
 end
